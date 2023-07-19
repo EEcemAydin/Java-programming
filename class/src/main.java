b13b2ef9b4c8475d553c768ba57c4715f7766b39
Elif Ecem Aydın <ecemeliff@icloud.com>
2023-07-19 15:48:52 +0300

    Create main.java

---
+++ class/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+	public static void main(String[]args)
+	{
+		dort_islem dort_islem=new dort_islem();
+		int sonuc1=dort_islem.topla(3,4);
+		System.out.println("toplama:" +sonuc1);
+		int sonuc2=dort_islem.cıkar(7, 2);
+		System.out.println("cıkarma:" +sonuc2);
+		int sonuc3=dort_islem.carp(5,9);
+		System.out.println("carpma:" +sonuc3);
+		int sonuc4=dort_islem.bol(2,0);
+		System.out.println("bolme:" +sonuc4);
+		
+				
+		
+	}
+
+}
