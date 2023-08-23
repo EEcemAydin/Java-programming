6309a329b14899b9dea5295efeefffeb875e7e99
Elif Ecem Aydın <ecemeliff@icloud.com>
2023-08-23 20:13:21 +0300

    Create exercises_8.java

---
+++ exercises_8/src/exercises_8/exercises_8.java
@@ -0,0 +1,17 @@
+package exercises_8;
+import java.util.Scanner;
+public class exercises_8 {
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		String myString=scan.next();
+		int myInt=scan.nextInt();
+		scan.close();
+		System.out.println("myString is: " + myString);
+		System.out.println("myInt is: " + myInt);
+		
+		
+	}
+	
+		
+
+}
